c7e6371f35e109a59747ce3c4692a46dc1e18bec
Scott Clark <clarkbarz@me.com>
2013-05-28 12:19:53 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Where to Watch
+==============
+
+This is a Ruby on Rails application designed to reveal the best place on the web to watch your favorite television series new or old.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhereToWatch::Application.config.secret_key_base = '1d8a41086b78eee93c1fd14208f03a6d3f0078b72ffae516fe7be1b5dad1cc0d3c8798ec0d72fac9014c78e8854e435636e1ad9a4a9b800060873ca74d5dbb01'
