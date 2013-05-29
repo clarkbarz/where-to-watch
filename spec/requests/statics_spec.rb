1a900d7866b4afccc4f609a85d625c0e3cc241b8
Scott Clark <clarkbarz@me.com>
2013-05-29 12:48:57 -0600

    Cleaned up layout, added sign up button on homepage

--- spec/requests/statics_spec.rb
+++ spec/requests/statics_spec.rb
@@ -20,4 +20,16 @@ describe "Static" do
   	it { should have_content("About #{base_title}") }
   	it { should have_title(full_title('About')) }
   end
+
+  it "should have the right link destinations" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_content("About #{base_title}")
+    click_link "Sign in"
+    expect(page).to have_content("Sign in")
+    click_link "Where to Watch"
+    expect(page).to have_content("Where to Watch")
+    click_link "Sign up"
+    expect(page).to have_content("Sign up")
+  end
 end
