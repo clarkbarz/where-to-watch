501aacc899981fe9768edda7ff4af8a35e833063
Scott Clark <clarkbarz@me.com>
2013-05-30 12:39:49 -0600

    tests for error messages

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,13 +16,20 @@ describe "User pages" do
    		it "should not create a user" do
    			expect { click_button submit }.not_to change(User, :count)
    		end
+
+   		describe "after submission" do
+   			before { click_button submit }
+
+   			it { should have_title('Sign up') }
+   			it { should have_content('error') }
+   		end
    	end
 
    	describe "with valid information" do
    		before do
  				fill_in "Email", with: "example@friend.com"
  				fill_in "Password", with: "uglygoat"
- 				fill_in "Password Confirmation", with: "uglygoat"
+ 				fill_in "Confirmation", with: "uglygoat"
  			end
 			it "should create a user" do
    			expect { click_button submit }.to change(User, :count)
