23f3351b229d8046e42836e7502269b765be9c42
Scott Clark <clarkbarz@me.com>
2013-05-30 12:09:19 -0600

    finished sign up page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+	factory :user do
+		email "clarkbarz@me.com"
+		password "friendlydragon"
+		password_confirmation "friendlydragon"
+	end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,36 @@ describe "User pages" do
 
   subject { page }
 
-   describe "signup page" do
+   describe "signup" do
    	before { visit signup_path }
+
+   	let(:submit) { "Create Account!" }
 
    	it { should have_content('Sign up') }
    	it { should have_title(full_title('Sign up')) }
+
+   	describe "with invalid information" do
+   		it "should not create a user" do
+   			expect { click_button submit }.not_to change(User, :count)
+   		end
+   	end
+
+   	describe "with valid information" do
+   		before do
+ 				fill_in "Email", with: "example@friend.com"
+ 				fill_in "Password", with: "uglygoat"
+ 				fill_in "Password Confirmation", with: "uglygoat"
+ 			end
+			it "should create a user" do
+   			expect { click_button submit }.to change(User, :count)
+   		end
+   	end
+  end
+
+  describe "profile page" do
+  	let(:user) { FactoryGirl.create(:user) }
+  	before { visit user_path(user) }
+
+  	it { should have_content(user.email) }
   end
 end
