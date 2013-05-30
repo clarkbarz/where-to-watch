0acf59462ae20c1dab973924a8ca361dff752df9
Scott Clark <clarkbarz@me.com>
2013-05-30 00:24:11 -0600

    layout changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 WhereToWatch::Application.routes.draw do
   get "users/new"
+  resources :users
   root to: 'static#home'
   match '/signup', to: 'users#new', via: 'get'
   match '/about', to: 'static#about', via: 'get'
