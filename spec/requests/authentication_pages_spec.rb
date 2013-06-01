75ed591fa90fae19157bae6fdccf06547f773cb5
Scott Clark <clarkbarz@me.com>
2013-06-01 13:40:07 -0600

    added tests for sign out button

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -35,11 +35,16 @@ describe "AuthenticationPages" do
   			click_button "Sign in"
   		end
 
+      it { should have_content(user.email) }
   		it { should have_link('Settings', href: user_path(user)) }
   		it { should have_link('Sign out', href: signout_path) }
   		it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
   	end
   end
-
 
 end
