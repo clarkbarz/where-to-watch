9fe43eb0a1c6c34b7b3aedc402c12865735c980d
Scott Clark <clarkbarz@me.com>
2013-06-13 16:52:45 -0600

    passing tests for wrong users accessing edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
 	def show
 		@user = User.find(params[:id])
@@ -21,11 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.authenticate(params[:user][:old_password])
       if @user.email != params[:user][:email]
         @user.update_attribute :email, params[:user][:email]
@@ -47,15 +46,21 @@ class UsersController < ApplicationController
   		params.require(:user).permit(:email, :password, :password_confirmation)
   	end
 
-    # Before filter
-
-    def signed_in_user
-      redirect_to signin_url, notice: "Please sign in. " unless signed_in?
-    end
-
     def sign_in_redirect_to(user)
       flash[:success] = "Profile updated"
       sign_in user
       redirect_to user
     end
+
+    # Before filters
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in. " unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
 end
